e4334352e2e4702685808a9303a512c8492784f8
vladli <73496636+vladli@users.noreply.github.com>
2021-05-06 05:26:41 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+const mySecret = process.env["TOKEN"];
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+
+// ================= START BOT CODE ===================
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong!");
+  }
+});
+
+client.login(mySecret);
