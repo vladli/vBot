00fde24c0873e81a33f6ad923c9908cf3bd2bba6
vladli <73496636+vladli@users.noreply.github.com>
2021-05-06 05:42:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const mySecret = process.env["TOKEN"];
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhostss:${port}`)
 );
 
 // ================= START BOT CODE ===================
